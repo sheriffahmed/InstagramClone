6730421f28f02478689542682c358d15c8dc4044
sheriffahmed <sheriff.v.ahmed@gmail.com>
2017-07-02 00:46:53 -0400

    part 19 & 20

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ var moment = require('moment');
 var mongoose = require('mongoose');
 var path = require('path');
 var request = require('request');
+var compress = require('compression');
 
 var config = require('./config');
 var User = mongoose.model('User', new mongoose.Schema({
@@ -24,10 +25,11 @@ mongoose.connect(config.db);
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
+app.use(compress());
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'), { maxAge: 2628000000 }));
 
 function createToken(user) {
   var payload = {
